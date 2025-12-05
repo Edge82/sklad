c798c70ddef3be2ddcee32f8cd517d32db8c8a3e
Alex Krai <krai_demon@mail.ru>
2025-12-05 03:05:51 +0300

    Fix vite config for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  base: '/sklad/',
   resolve: {
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
@@ -15,6 +16,9 @@ export default defineConfig({
     open: true
   },
   build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    emptyOutDir: true,
     target: 'esnext',
     minify: 'esbuild'
   }
